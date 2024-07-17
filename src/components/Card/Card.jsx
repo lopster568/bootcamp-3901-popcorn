d8f496a2e51c7c3d638d26aea2b7d88ae2cf2f71
Roshan Singh <rosh.s568@gmail.com>
2024-07-17 23:52:59 +0530

    Card Overlay Fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,17 +6,17 @@ const Card = ({ movie }) => {
         <Link className="card__link" to={`/movie/${movie?.id}`} >
             <div className="card" >
                 <img src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} className="card__image" alt="" />
-            </div>
-            <div className="card__overlay" >
-                <div className="card__title" >
-                    {movie?.title}
-                </div>
-                <div className="card__runtime" >
-                    {movie?.release_date}
-                    <span className="card__rating" >{movie?.vote_average} ⭐</span>
-                </div>
-                <div className="card__description" >
-                    {movie?.overview.slice(0, 118)}
+                <div className="card__overlay" >
+                    <div className="card__title" >
+                        {movie?.title}
+                    </div>
+                    <div className="card__runtime" >
+                        {movie?.release_date}
+                        <span className="card__rating" >{movie?.vote_average} ⭐</span>
+                    </div>
+                    <div className="card__description" >
+                        {movie?.overview.slice(0, 118)}
+                    </div>
                 </div>
             </div>
         </Link>
